255a599ec27d312801db7be67274b534a0f41524
29886071 <Mallikarjun.Patil@Wkk.IN.ecw.local>
2020-08-16 11:09:00 -0400

    XMLHttpRequest

--- hangman/app.js
+++ hangman/app.js
@@ -17,3 +17,15 @@ window.addEventListener('keypress',function(e){
 }
 )
 
+const request = new XMLHttpRequest()
+request.open('GET','http://restcountries.eu/rest/v2/all')
+request.send()
+request.addEventListener('readystatechange',(e)=>{
+
+    if(e.target.readyState === 4 && e.target.status === 200)
+    {   
+        const data= JSON.parse(e.target.responseText)
+        const country= data.find((country) => country.alpha2Code === 'IN')
+        console.log(country.name);
+    }
+})
