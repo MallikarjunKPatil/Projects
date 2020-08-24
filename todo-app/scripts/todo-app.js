33814baa6fc35171aecea0fd14303fdb162c0ef0
29886071 <mallikarjunpatil44@gmail.com>
2020-08-24 10:10:41 -0400

    todo

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -67,7 +67,6 @@ document.querySelector('#input-form').addEventListener('submit', (e) => {
 
 // Create a checkbox and setup event listener -> "Hide completed"
 document.querySelector('#checkboxID').addEventListener('change', (e) => {
-    console.log(e.target.checked);
     filters.hideCompleted = e.target.checked
     renderTods(todos, filters)
 })
